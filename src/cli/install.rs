767299ef124a4de56d88905f41a38da8d5e5b8a6
Jermiah Joseph <jermiahjoseph98@gmail.com>
2024-05-17 14:02:27 -0400

    feat: Install all envs

--- src/cli/install.rs
+++ src/cli/install.rs
@@ -1,5 +1,7 @@
 use crate::config::ConfigCli;
 use crate::environment::get_up_to_date_prefix;
+use crate::progress::await_in_progress;
+use crate::project::Environment;
 use crate::Project;
 use clap::Parser;
 use std::path::PathBuf;
@@ -10,6 +12,10 @@ pub struct Args {
     /// The path to 'pixi.toml' or 'pyproject.toml'
     #[arg(long)]
     pub manifest_path: Option<PathBuf>,
+
+    /// Install All Environments
+    #[arg(long, short)]
+    pub all: bool,
 
     #[clap(flatten)]
     pub lock_file_usage: super::LockFileUsageArgs,
@@ -24,16 +30,33 @@ pub struct Args {
 pub async fn execute(args: Args) -> miette::Result<()> {
     let project =
         Project::load_or_else_discover(args.manifest_path.as_deref())?.with_cli_config(args.config);
-    let environment = project.environment_from_name_or_env_var(args.environment)?;
+        
+    let envs: Vec<Environment> = if args.all {
+        project.environments()
+    } else {
+        vec![project.environment_from_name_or_env_var(args.environment)?]
+    };
 
-    get_up_to_date_prefix(&environment, args.lock_file_usage.into(), false).await?;
+    for environment in envs.iter() {
+        await_in_progress(
+            format!("Installing environment {}...", environment.name()),
+            |_| get_up_to_date_prefix(&environment, args.lock_file_usage.into(), false),
+        )
+        .await?;
 
-    // Emit success
+        eprintln!(
+            "{}Environment {} is ready to use!",
+            console::style(console::Emoji("✔ ", "")).green(),
+            environment.name()
+        );
+    }
+
     eprintln!(
         "{}Project in {} is ready to use!",
         console::style(console::Emoji("✔ ", "")).green(),
         project.root().display()
     );
+
     Project::warn_on_discovered_from_env(args.manifest_path.as_deref());
     Ok(())
 }
